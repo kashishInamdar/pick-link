3629811306b2a82e15676ef662dd25b2a9423e76
kashishInamdar <kashishinamdar2004@gmail.com>
2023-11-24 00:34:49 +0530

    sum basic chages

--- server/index.js
+++ server/index.js
@@ -93,10 +93,10 @@ app.get("/api/links", async (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client2', 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client2', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
   });
 }
 
